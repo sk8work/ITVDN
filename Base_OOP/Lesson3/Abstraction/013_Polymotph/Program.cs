77445cf31009a1083cbb43b51e8345703a381658
sk8work <eshikhantsev@gmail.com>
2021-03-30 05:52:48 +0300

    rebuild lesson 3

--- Base_OOP/Lesson3/Abstraction/013_Polymotph/Program.cs
+++ Base_OOP/Lesson3/Abstraction/013_Polymotph/Program.cs
@@ -2,11 +2,30 @@
 
 namespace _013_Polymotph
 {
+    class BaseClass { }
+
+    class Derived1 : BaseClass { }
+
+    class Derived2 : BaseClass { }
+
+    class Container
+    {
+        public BaseClass field;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Container container = new Container();
+
+            container.field = new Derived1();
+            Console.WriteLine(container.field.GetHashCode());
+
+            container.field = new Derived2();
+            Console.WriteLine(container.field.GetHashCode());
+
+            // Delay
+            Console.ReadKey();
         }
     }
 }
