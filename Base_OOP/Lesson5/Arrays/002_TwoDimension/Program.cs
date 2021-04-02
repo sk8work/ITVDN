1c897bec51c8473a9bb4e846a5b5e63cee1260ed
sk8work <eshikhantsev@gmail.com>
2021-04-03 01:36:31 +0300

    BaseOOP_Lesson5_added_002_TwoDimention

---
+++ Base_OOP/Lesson5/Arrays/002_TwoDimension/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Arrays (2d array)
+
+namespace _002_TwoDimension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[,] array = new int[3, 3];
+
+            // fill array by randoms
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    array[i, j] = random.Next(0, 10);
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("{0}", array[i,j]);
+                }
+            }
+
+        }
+    }
+}
