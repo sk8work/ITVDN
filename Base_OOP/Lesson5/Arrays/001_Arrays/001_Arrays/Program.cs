81445641c27e1c4b785c9a55e2ab4a69821410fa
sk8work <eshikhantsev@gmail.com>
2021-04-03 01:28:29 +0300

    BaseOOP_Lesson5_added_001_arrays

---
+++ Base_OOP/Lesson5/Arrays/001_Arrays/001_Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Arrays (1-Direction array)
+
+namespace _001_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            array = new int[5];
+
+            array = new int[5] { 1, 2, 3, 4, 5 };
+
+            // Get elems of array
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
