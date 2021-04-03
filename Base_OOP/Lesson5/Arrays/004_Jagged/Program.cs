73464379a67b5e887347e05f5947f45d6d6994da
sk8work <eshikhantsev@gmail.com>
2021-04-03 05:43:40 +0300

    BaseOOP_Lesson5_added_004_Jagged

---
+++ Base_OOP/Lesson5/Arrays/004_Jagged/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Jagged Arrays
+
+namespace _004_Jagged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged = new int[3][];
+
+            jagged[0] = new int[] { 1, 2 };
+            jagged[1] = new int[] { 1, 2, 3, 4, 5 };
+            jagged[2] = new int[] { 1, 2, 3 };
+
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    Console.Write("{0}", jagged[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
