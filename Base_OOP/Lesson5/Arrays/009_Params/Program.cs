91305665c0994f47b079bfff5ad6ad1f96b25482
sk8work <eshikhantsev@gmail.com>
2021-04-03 06:19:54 +0300

    BaseOOP_Lesson5_added_009_Params

---
+++ Base_OOP/Lesson5/Arrays/009_Params/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Параметр с ключевым словом params ддолжен бфть только один и стоять последним в списке параметров метода
+
+namespace _009_Params
+{
+    class Program
+    {
+        static void ShowArray(string name, params int[] array)
+        {
+            Console.Write(name);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0}", array[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ShowArray("Numbers", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
