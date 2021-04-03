4948a60d6068208ae94e23d38f5e6a11fa33cca5
sk8work <eshikhantsev@gmail.com>
2021-04-03 06:15:45 +0300

    BaseOOP_Lesson5_added_008_Params

---
+++ Base_OOP/Lesson5/Arrays/008_Params/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Ключевое слово params позволяет определить параметр метода, приниающий переменное количество параметров
+
+namespace _008_Params
+{
+    class Program
+    {
+       static void ShowArray(params int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("{0}", array[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ShowArray(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
