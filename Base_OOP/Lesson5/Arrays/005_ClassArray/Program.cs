f8456d438028d81dcc9ae6e0ed378f7cf98c618a
sk8work <eshikhantsev@gmail.com>
2021-04-03 05:49:36 +0300

    BaseOOP_Lesson5_added_005_ClassArray

---
+++ Base_OOP/Lesson5/Arrays/005_ClassArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Abstract class Array
+
+// All arrays are derived from class Array
+
+namespace _005_ClassArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = { 1, 2, 3 };
+
+            Array array = vector as Array;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
