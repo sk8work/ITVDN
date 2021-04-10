7a113360a611f722b771c9f2483859539031a2fb
sk8work <eshikhantsev@gmail.com>
2021-04-10 04:23:04 +0300

    Base OOP Lesson_5 001_Indexers

---
+++ Base_OOP/Lesson5/Indexers/001_Indexers/001_Indexers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// Indexers
+
+namespace _001_Indexers
+{
+    class MyClass
+    {
+        private int[] array = new int[5];
+
+        // Indexer
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+
+            my[0] = 1;
+            my[1] = 2;
+            my[2] = 3;
+            my[3] = 4;
+            my[4] = 5;
+
+            Console.WriteLine(my[0]);
+            Console.WriteLine(my[1]);
+            Console.WriteLine(my[2]);
+            Console.WriteLine(my[3]);
+            Console.WriteLine(my[4]);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
