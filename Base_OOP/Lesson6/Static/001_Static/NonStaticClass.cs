de77d883b22172b6e99efc628b0d0fc3160b5e64
sk8work <eshikhantsev@gmail.com>
2021-04-10 06:03:57 +0300

    Base OOP Lesson_6 001_Static

---
+++ Base_OOP/Lesson6/Static/001_Static/NonStaticClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _001_Static
+{
+    class NonStaticClass
+    {
+        private int Id;
+        public static int field;
+
+        // Constructor
+        public NonStaticClass(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Instance{0}.field = {1}", Id, field);
+        }
+    }
+}
---
+++ Base_OOP/Lesson6/Static/001_Static/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Static members from nonstatic classes
+
+namespace _001_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NonStaticClass instance1 = new NonStaticClass(1);
+            NonStaticClass instance2 = new NonStaticClass(2);
+
+            instance1.Method();
+            instance2.Method();
+
+            NonStaticClass.field = 1;
+
+            instance1.Method();
+            instance2.Method();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
