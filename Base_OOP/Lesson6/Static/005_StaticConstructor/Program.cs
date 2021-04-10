4db78fd0a90f938b2fbedc610967e2bc94c3199a
sk8work <eshikhantsev@gmail.com>
2021-04-11 02:43:36 +0300

    Base OOP Lesson_6 005_StaticConstructor

---
+++ Base_OOP/Lesson6/Static/005_StaticConstructor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Static construtor
+
+namespace _005_StaticConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NotStaticClass.ReadonlyField);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
