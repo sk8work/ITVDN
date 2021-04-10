08d6d8963e51fee0f2eddf2f46863b74bcf54caa
sk8work <eshikhantsev@gmail.com>
2021-04-10 04:33:50 +0300

    Base OOP Lesson_5 002_Indexers

---
+++ Base_OOP/Lesson5/Indexers/002_Indexers/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Indexers
+{
+    class MyClass
+    {
+        private string[] array = new string[5];
+
+        // Indexer
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                    return array[index];
+                else
+                    return "Попытка обращения за пределы массива";
+            }
+            set
+            {
+                if (index >= 0 && index < array.Length)
+                    array[index] = value;
+                else
+                    Console.WriteLine("Попытка записи за пределамми массива");
+            }
+        }
+    }
+}
---
+++ Base_OOP/Lesson5/Indexers/002_Indexers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Indexers
+
+namespace _002_Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+
+            my[0] = "string1";
+            my[1] = "string2";
+            my[2] = "string3";
+            my[3] = "string4";
+            my[4] = "string5";
+            my[5] = "string6";
+
+            Console.WriteLine(my[0]);
+            Console.WriteLine(my[1]);
+            Console.WriteLine(my[2]);
+            Console.WriteLine(my[3]);
+            Console.WriteLine(my[4]);
+            Console.WriteLine(my[5]);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
