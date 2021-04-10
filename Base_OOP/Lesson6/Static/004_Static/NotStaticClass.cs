661e968efa4c597c76f10ad4cc21cb3ffefd9270
sk8work <eshikhantsev@gmail.com>
2021-04-11 02:29:57 +0300

    Base OOP Lesson_6 004_Static

---
+++ Base_OOP/Lesson6/Static/004_Static/NotStaticClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Статические поля и свойства
+
+namespace _004_Static
+{
+    class NotStaticClass
+    {
+        // Статическое поле
+        static int field;
+
+        // Статическое свойство
+        public static int Property
+        {
+            get { return field; }
+            set { field = value; }
+        }
+    }
+}
---
+++ Base_OOP/Lesson6/Static/004_Static/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Статические члены в нестатических классах
+
+namespace _004_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotStaticClass.Property = 33;
+            Console.WriteLine(NotStaticClass.Property);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
