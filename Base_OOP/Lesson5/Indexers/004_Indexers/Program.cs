7796fc314f64d67de17acd1d318bc5bbaec8b4b2
sk8work <eshikhantsev@gmail.com>
2021-04-10 04:46:35 +0300

    Base OOP Lesson_5 004_Indexers

---
+++ Base_OOP/Lesson5/Indexers/004_Indexers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Indexers
+
+namespace _004_Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+
+            my[1, 1] = 2;
+
+            Console.WriteLine(my[1,1]);
+            Console.WriteLine(my[0,0]);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
