af1a856b9d97adfe8248bde9d327fc4906812aa7
sk8work <eshikhantsev@gmail.com>
2021-04-11 02:23:45 +0300

    Base OOP Lesson_6 003_Static

---
+++ Base_OOP/Lesson6/Static/003_Constant/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _003_Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("e = {0}", NotStaticClass.e);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
