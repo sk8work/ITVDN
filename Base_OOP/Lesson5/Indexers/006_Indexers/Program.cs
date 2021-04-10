d3c7667150636888ee1789856fb28cc91ef71b77
sk8work <eshikhantsev@gmail.com>
2021-04-10 05:10:09 +0300

    Base OOP Lesson_5 005_Indexers

---
+++ Base_OOP/Lesson5/Indexers/006_Indexers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// Indexers
+
+namespace _005_Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary dict = new Dictionary();
+
+            Console.WriteLine(dict["книга"]);
+            Console.WriteLine(dict["дом"]);
+            Console.WriteLine(dict["ручка"]);
+            Console.WriteLine(dict["стол"]);
+            Console.WriteLine(dict["карандаш"]);
+            Console.WriteLine(dict["яблоко"]);
+            Console.WriteLine(dict["солнце"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dict[i]);
+            }
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
