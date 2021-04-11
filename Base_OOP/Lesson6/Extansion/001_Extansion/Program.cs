6f7102f39f31c58316bce00a470314f2b3bec16e
sk8work <eshikhantsev@gmail.com>
2021-04-11 04:44:55 +0300

    Base OOP Lesson_6 001_Extension

---
+++ Base_OOP/Lesson6/Extansion/001_Extansion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// Расширяющие методы (Extansion methods)
+
+// Расширяющие методы могут быть только статическими и создаваться только в статических классах
+
+namespace _001_Extansion
+{
+    static class ExtansionClass
+    {
+        // this - сообщает компилятору, что данный метод является расширяющим (Extansion)!
+        public static void ExtansionMethod(this string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Тестовая строка";
+
+            //Вызов метода, как статического
+            ExtansionClass.ExtansionMethod(text);
+
+            // Вызов метода, как расширяющего
+            text.ExtansionMethod();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
