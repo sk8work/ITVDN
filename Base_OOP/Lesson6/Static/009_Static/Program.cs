7d0c3e1cd55006b5a6b98d0b3790ec8113125427
sk8work <eshikhantsev@gmail.com>
2021-04-11 03:30:17 +0300

    Base OOP Lesson_6 009_Static

---
+++ Base_OOP/Lesson6/Static/009_Static/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+// Статические члены не могут быть виртуальными, переопределенными и абстрактными
+
+// Статические члены поддерживают замещение
+
+namespace _009_Static
+{
+    abstract class BaseClass
+    {
+        /*public static virtual void StaticMethod()
+        {
+            Console.WriteLine("BaseClass.StaticMethod");
+        }*/
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("BaseClass.StaticMethod");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        /*public static override void StaticMethod()
+        {
+            Console.WriteLine("DerivedClass.StaticMethod");
+        }*/
+
+        public static new void StaticMethod()
+        {
+            Console.WriteLine("DerivedClass.StaticMethod");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass.StaticMethod();
+            DerivedClass.StaticMethod();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
