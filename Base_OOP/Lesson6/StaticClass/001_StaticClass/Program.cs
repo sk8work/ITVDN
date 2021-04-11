dae39824453d45e1918e1bd7683cd71b8628a9da
sk8work <eshikhantsev@gmail.com>
2021-04-11 04:07:52 +0300

    Base OOP Lesson_6 001_StaticClass

---
+++ Base_OOP/Lesson6/StaticClass/001_StaticClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Статические классы
+
+namespace _001_StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticClass.StaticMethod();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
