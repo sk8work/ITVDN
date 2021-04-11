54f757c126dd62aee515388a9140974f2e538c60
sk8work <eshikhantsev@gmail.com>
2021-04-11 03:00:24 +0300

    Base OOP Lesson_6 006_StaticConstructor

---
+++ Base_OOP/Lesson6/Static/006_StaticConstructor/NotStaticClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _006_StaticConstructor
+{
+    class NotStaticClass
+    {
+        // Если класс содержит статические поля, должен быть предоставлен статический конструктор,
+        // который инициализирует эти поля при загрузке класса
+
+        private static int field;
+
+        public static int Property
+        {
+            get { return field; }
+            set { field = value; }
+        }
+
+        // Статический конструктор 
+        // Единствунное назначение статических конструкторов - присваивать исходные значения статическим полям
+        static NotStaticClass()
+        {
+            Console.WriteLine("Ствтический конструктор - NotStaticClass()");
+            field = 1;
+        }
+
+        // Статические методы мгут быть перегружены
+        public static void Method()
+        {
+            Console.WriteLine("Статический метод нестатического класса NotStaticClass");
+        }
+
+        // Статические метода могут быть перегружены
+        public static void Method(int s)
+        {
+            Console.WriteLine("Перегруженный статический метод нестатического класса NotStaticClass " + s);
+        }
+
+        // Нестатический метод 
+        public void NotStaticMethod()
+        {
+            Console.WriteLine(field);
+        }
+    }
+}
---
+++ Base_OOP/Lesson6/Static/006_StaticConstructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Сьаьические члены в нестатических классах
+
+namespace _006_StaticConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotStaticClass instance = new NotStaticClass();
+            instance.NotStaticMethod();
+
+            NotStaticClass.Property = 2;
+            Console.WriteLine(NotStaticClass.Property);
+
+            NotStaticClass.Method();
+            NotStaticClass.Method(3);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
