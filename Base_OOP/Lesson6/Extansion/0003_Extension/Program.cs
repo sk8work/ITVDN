5ecee3feba79a37d050d906cea1723a2c0e62340
sk8work <eshikhantsev@gmail.com>
2021-04-11 04:58:43 +0300

    Base OOP Lesson_6 003_Extension

---
+++ Base_OOP/Lesson6/Extansion/0003_Extension/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Расширяющие методы (Extension methods)
+
+// Расширяющие методы не могут иметь опциональных параметров, но могу быть перегружены
+
+namespace _003_Extension
+{
+    static class ExtentionClass
+    {
+        public static void ExtensionMethod(this string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public static void ExtensionMethod(this string value1, string value2)
+        {
+            Console.WriteLine(value1 + value2);
+        }
+
+        public static void ExtensionMethod(this int value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello ";
+            text.ExtensionMethod("World!");
+
+            "Hello ".ExtensionMethod("world!");
+            2.ExtensionMethod();
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
