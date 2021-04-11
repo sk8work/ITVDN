c9a56d70e6f4a0a82c40c596c94765535e10a6d3
sk8work <eshikhantsev@gmail.com>
2021-04-11 03:10:21 +0300

    Base OOP Lesson_6 007_Constructors

---
+++ Base_OOP/Lesson6/Static/007_Constructors/NotStaticClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _007_Constructors
+{
+    class NotStaticClass
+    {
+        // Конструктор экземпляра вызывается только при создании экемпляра класса
+        public NotStaticClass()
+        {
+            Console.WriteLine("Constructor");
+        }
+
+        // Статический конструктор вызывается в любом случае
+        // как при обращении к статическому члену, так и при создании экземпляяра класса
+        static NotStaticClass()
+        {
+            Console.WriteLine("StaticConstructor");
+        }
+
+        // Статический метод
+        public static void StaticMethod()
+        {
+            Console.WriteLine("StaticMethod");
+        }
+
+        public void NotStaticMethod()
+        {
+            Console.WriteLine("NotStaticMethod");
+        }
+    }
+}
---
+++ Base_OOP/Lesson6/Static/007_Constructors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// Статический конструктор
+
+namespace _007_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Вариант (Вызывается только СТАТИЧЕСКИЙ конструктор)
+            // NotStaticClass.StaticMethod();
+
+            // 2. Вариант (Вызываются оба конструктора)
+            new NotStaticClass().NotStaticMethod();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
