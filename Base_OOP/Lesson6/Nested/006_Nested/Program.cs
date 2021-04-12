552c1988a262b63acb46303064be7759cbac4bca
sk8work <eshikhantsev@gmail.com>
2021-04-12 18:04:54 +0300

    Base_OOP_Lesson6_Nested_006_Nested

---
+++ Base_OOP/Lesson6/Nested/006_Nested/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Nested classes
+
+// Статические классы могут в себе содержать нестадические Nested классы
+
+namespace _006_Nested
+{
+    public static class MyClass
+    {
+        static MyClass()
+        {
+            Console.WriteLine("Статический конструктор MyClass");
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Статический метод класса MyClass");
+        }
+
+        public class Nested
+        {
+            public void MethodFromNested()
+            {
+                Console.WriteLine("Метод Nested класса");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.StaticMethod();
+
+            MyClass.Nested instance = new MyClass.Nested();
+            instance.MethodFromNested();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
