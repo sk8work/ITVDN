e2eed6a80e84bdecd5114d78be9aefc5a83869c5
sk8work <eshikhantsev@gmail.com>
2021-04-12 17:02:51 +0300

    Base_OOP_Lesson6_Nested_001_Nested

---
+++ Base_OOP/Lesson6/Nested/001_Nested/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// Nested classes
+
+namespace _001_Nested
+{
+    class MyClass
+    {
+        public class Nested
+        {
+            public void Method()
+            {
+                Console.WriteLine("Метод из Nested класса");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.Nested instance = new MyClass.Nested();
+
+            instance.Method();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
